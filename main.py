6b77294cefc88d6b3b7118a797fd41e7087379c9
Narongrit159 <narongrit133@gmail.com>
2024-02-07 21:36:57 +0700

    Narongrit

--- main.py
+++ main.py
@@ -2,36 +2,30 @@
 from fastapi import FastAPI, Depends, HTTPException, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from models import User, Product, Controller
+from models import Account, Product, Controller
 from fastapi import Request
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-# Mock data for demonstration purposes
-fake_user_db = {
-    "chicken": User(username="chicken", password="0000"),
-    "user2": User(username="user2", password="pass2"),
-}
+chick_shop = Controller()
+chick_shop.add_account(Account("chicken", "0000"))
 
-fake_product_db = {
-    "product1": Product(name="Product 1", price=19),
-    "product2": Product(name="Product 2", price=28),
-    "product3": Product(name="Product 3", price=37),
-    "product4": Product(name="Product 4", price=46),
-    "product5": Product(name="Product 5", price=55),
-    "product6": Product(name="Product 6", price=64),
-    "product7": Product(name="Product 7", price=73),
-    "product8": Product(name="Product 8", price=82),
-    "product9": Product(name="Product 9", price=91),
-}
+chick_shop.add_product(Product(name="Product 1", price=19))
+chick_shop.add_product(Product(name="Product 2", price=28))
+chick_shop.add_product(Product(name="Product 3", price=37))
+chick_shop.add_product(Product(name="Product 4", price=46))
+chick_shop.add_product(Product(name="Product 5", price=55))
+chick_shop.add_product(Product(name="Product 6", price=64))
+chick_shop.add_product(Product(name="Product 7", price=73))
+chick_shop.add_product(Product(name="Product 8", price=82))
+chick_shop.add_product(Product(name="Product 9", price=91))
 
 
-def get_current_user(username: str):
-    user = fake_user_db.get(username)
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+def get_current_user(username: str, password: str):
+    if chick_shop.validate_user_credentials(username, password):
+        return chick_shop.get_user_by_username(username)
+    raise HTTPException(status_code=404, detail="User not found or invalid credentials")
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -40,12 +34,18 @@ def get_home(request: Request):
 
 
 @app.post("/", response_class=HTMLResponse)
-def post_home(request: Request, username: str = Form(...)):
-    user = fake_user_db.get(username)
-    if user is None:
-        return templates.TemplateResponse("login.html", {"request": request})
-
-    return templates.TemplateResponse(
-        "products.html",
-        {"request": request, "user": username, "products": fake_product_db.values()},
-    )
+def post_home(request: Request, username: str = Form(...), password: str = Form(...)):
+    try:
+        user = get_current_user(username, password)
+        return templates.TemplateResponse(
+            "products.html",
+            {
+                "request": request,
+                "user": user.username,
+                "products": chick_shop.get_product(),
+            },
+        )
+    except HTTPException as e:
+        return templates.TemplateResponse(
+            "login.html", {"request": request, "error_message": str(e.detail)}
+        )
--- models.py
+++ models.py
@@ -1,14 +1,55 @@
 class Controller:
-    pass
+    def __init__(self):
+        self.__account_list = []
+        self.__promotion_list = []
+        self.__product_list = []
+        self.__history_order_list = []
+
+    def add_account(self, account):
+        self.__account_list.append(account)
+
+    def add_product(self, product):
+        self.__product_list.append(product)
+
+    def get_user_by_username(self, username):
+        for account in self.__account_list:
+            if account.username == username:
+                return account
+            return None
+
+    def validate_user_credentials(self, username, password):
+        user = self.get_user_by_username(username)
+        if user and user.password == password:
+            return True
+        return False
+
+    def get_product(self):
+        return self.__product_list
 
 
-class User:
+class Account:
     def __init__(self, username, password):
-        self.username = username
-        self.password = password
+        self.__username = username
+        self.__password = password
+
+    @property
+    def username(self):
+        return self.__username
+
+    @property
+    def password(self):
+        return self.__password
 
 
 class Product:
     def __init__(self, name, price):
-        self.name = name
-        self.price = price
+        self.__name = name
+        self.__price = price
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def price(self):
+        return self.__price
