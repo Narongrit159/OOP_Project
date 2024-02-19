fecacfa49bdd46af49b2e2bc0b822c2b4785a380
Teerachat888 <159001021+Teerachat888@users.noreply.github.com>
2024-02-19 14:12:30 +0700

    tar

--- models.py
+++ models.py
@@ -113,9 +113,35 @@ class Controller:
         for promotion in self.__promotion_list:
             if promotion.promotion_id == promotion_id:
                 return promotion
+            
+    def search_payment_by_id(self, payment_id):
+        for payment in self.__payment_list:
+            if payment_id == payment.id:
+                return payment
 
-    def calculate_order_price(self, username):
-        pass
+    def check_quanity_product(self, selected_product_list):########################New method
+        for selected_product in selected_product_list:
+            for product in self.__product_list:
+                if selected_product.quanity > product.quanity:
+                    return False
+        return True
+    
+    def calculate_order_price(self,total_price,discount):
+        order_price = total_price - discount
+        return order_price
+                     
+    def create_order(self, username, address_id, promotion, payment_id):
+        account = self.search_account_by_username(username)
+        cart    = account.get_cart
+        selected_product_list = cart.show_selected_product_list
+        check_selected_product = self.check_quanity_product(selected_product_list)
+        if check_selected_product == True:
+            order_id = self.get_last_history_id()
+            payment = self.search_payment_by_id(payment_id)
+            promotion = self.search_promotion_by_id(promotion_id)
+            order = Order(order_id,selected_product_list,
+                          order_id,selected_product_list,order_price,payment,account,address,tel,status
+        
 
 
 class Account:
@@ -344,19 +370,9 @@ class Promotion:
 
 
 class Order:
-    def __init__(
-        self,
-        order_id,
-        product_list,
-        order_price,
-        payment_type,
-        account,
-        address,
-        tel,
-        status,
-    ):
+    def __init__(self,order_id,selected_product_list,order_price,payment_type,account,address,tel,status,):
         self.__order_id = order_id
-        self.__product_list = product_list
+        self.__selected_product_list = selected_product_list
         self.__order_price = order_price
         self.__payment_type = payment_type
         self.__account = account
@@ -376,15 +392,15 @@ class Payment:
         self.__discount = discount
 
     @property
-    def get_id(self):
+    def id(self):
         return self.__payment_id
 
     @property
-    def get_payment_type(self):
+    def payment_type(self):
         return self.__payment_type
 
     @property
-    def get_discount(self):
+    def discount(self):
         return self.__discount
 
 
